f3aec7b055d19c2b72069e142a0044e322339758
Bruno Jurkovski <bjurkovski@gmail.com>
2010-08-11 21:19:44 -0300

    fixed readmap and json file

--- readmap.py
+++ readmap.py
@@ -1,35 +1,32 @@
 import json
 
+class Map:
+	def __init__(self, mapFile):
+		file = open(filename)
+		self.tiles = []
+		for line in file.readlines():
+			self.tiles.append([c for c in line if c != '\n'])
+		file.close()
+
+	def __str__(self):
+		str = ""
+		for row in self.tiles:
+			for tile in row:
+				str += tile
+			str += "\n"
+		return str
+
 class Stage:
 	def __init__(self, stageFile):
 		file = open(stageFile)
 		self.data = json.loads(file.read())
+		self.maps = {}
+		for room in self.data["rooms"]:
+			self.maps[room] = Map(self.data["rooms"][room]["map"])
 		file.close()
-	
-	def printRoom(self, roomId)
-		#str = ""
-		#for row in self.data:
-		#	for tile in row:
-		#		str += tile
-		#	str += "\n"
-		#print(str)
+		
 
 
-filename = "map.txt"
-	
+filename = "stage.txt"
 
-def readmap(filename):
-	file = open(filename)
-	map = []
-	for line in file.readlines():
-		map.append(list(c for c in line if c != '\n'))
-	file.close()
-	return map
-
-data = readjson("stage.txt")
-print data["stage-map"]
-print data["start"]
-for r in data["rooms"]:
-	print r
-#map = readmap(filename)
-#output(map)
+a = Stage(filename)	
