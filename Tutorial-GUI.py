89d425c41f4586f170b6e88c7781e8f85d16280a
Bruno Jurkovski <bjurkovski@gmail.com>
2010-08-14 15:15:28 -0300

    card texture working

--- Tutorial-GUI.py
+++ Tutorial-GUI.py
@@ -1,87 +1,38 @@
-from direct.directbase import DirectStart
-from direct.showbase.DirectObject import DirectObject
-from direct.gui.DirectGui import *
-from direct.interval.IntervalGlobal import *
-from panda3d.core import lookAt
+from math import pi, sin, cos
+ 
+from direct.showbase.ShowBase import ShowBase
+from direct.task import Task
+
 from panda3d.core import CardMaker
-from panda3d.core import TextNode
-import sys, os
+from direct.gui.DirectGui import *
+ 
+class MyApp(ShowBase):
+	def __init__(self):
+		ShowBase.__init__(self)
 
-title = OnscreenText(text="MapMaker",
-                       style=1, fg=(1,1,1,1),
-                       pos=(0.5,-0.95), scale = .07)
+		self.cm = CardMaker('card')
+		self.cm.setFrame(0.8,-0.8,0.8,-0.8)
+		self.cm.setColor(0,1,0,1)
+		# usando render2d, tudo que for desenhado vai ser desenhado como se fosse pintado na lente da camera
+		# (em cima de todo o resto ja desenhado). Coordenadas da tela no range [-1,1]
+		self.card = self.render2d.attachNewNode(self.cm.generate())
+		# eh possivel usar o render padrao (3d) tambem, mas dai seria necessario usar o TaskManager (vide abaixo)
+		#self.card = self.render2d.attachNewNode(self.cm.generate())
+		tex = loader.loadTexture('map.png')
+		self.card.setTexture(tex)
 
-cm = CardMaker('card')
-cm.setFrame(1,-1,1,-1)
-cm.setColor(0,1,0,0)
-card = render.attachNewNode(cm.generate())
+		# usando o render padrao (3d), eh necessario adicionar esse callback
+		# Aparentemente esse eh o segredo pra mostrar o Card na tela, mas nao descobri o pq...
+		#self.taskMgr.add(self.idle, "Idle")
 
-tex = loader.loadTexture('map.png')
-card.setTexture(tex)
+		self.title = OnscreenText(text="MapMaker",
+								  style=1, fg=(1,1,1,1),
+								  pos=(0.5,-0.95), scale = .07)
 
-#geom.setFrameFullscreenQuad(-1,1,-1,1)
-
-
-#escapeEvent = OnscreenText(
-# 			 text="1: Set a Texture onto the Cube",
-#     			 style=1, fg=(1,1,1,1), pos=(-1.3, 0.95),
-#			 align=TextNode.ALeft, scale = .05)
-#spaceEvent = OnscreenText(
-# 			 text="2: Toggle Light from the front On/Off",
-#     			 style=1, fg=(1,1,1,1), pos=(-1.3, 0.90),
-#			 align=TextNode.ALeft, scale = .05)
-#upDownEvent = OnscreenText(
-# 			 text="3: Toggle Light from on top On/Off",
-#     			 style=1, fg=(1,1,1,1), pos=(-1.3, 0.85),
-#			 align=TextNode.ALeft, scale = .05)
-
-#class MyTapper(DirectObject):
-#	def __init__(self):
-#		self.testTexture=loader.loadTexture("maps/envir-reeds.png")
-#		self.accept("1", self.toggleTex)
-#		self.accept("2", self.toggleLightsSide)
-#		self.accept("3", self.toggleLightsUp)
-
-#		self.LightsOn=False
-#		self.LightsOn1=False
-#		slight = Spotlight('slight')
-#		slight.setColor(Vec4(1, 1, 1, 1))
-#		lens = PerspectiveLens()
-#		slight.setLens(lens)
-#		self.slnp = render.attachNewNode(slight)
-#		self.slnp1= render.attachNewNode(slight)
-
-#	def toggleTex(self):
-#		global cube
-#		if cube.hasTexture():
-#			cube.setTextureOff(1)
-#		else:
-#			cube.setTexture(self.testTexture)
-
-#	def toggleLightsSide(self):
-#		global cube
-#		self.LightsOn=not(self.LightsOn)
-
-#		if self.LightsOn:
-#			render.setLight(self.slnp)
-#			self.slnp.setPos(cube, 10,-400,0)
-#			self.slnp.lookAt(Point3(10, 0, 0))
-#		else:
-#			render.setLightOff(self.slnp)
-
-#	def toggleLightsUp(self):
-#		global cube
-#		self.LightsOn1=not(self.LightsOn1)
-
-#		if self.LightsOn1:
-#			render.setLight(self.slnp1)
-#			self.slnp1.setPos(cube, 10,0,400)
-#			self.slnp1.lookAt(Point3(10, 0, 0))
-#		else:
-#			render.setLightOff(self.slnp1)
-
-
-#t=MyTapper()
-
-run()
-
+	def idle(self, task):
+		#se colocar esse setPos no __init__, nao funciona...
+		self.camera.setPos(0, -20.0, 0)
+		return Task.cont
+ 
+app = MyApp()
+app.run()
