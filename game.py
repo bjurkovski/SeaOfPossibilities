51a10599b2e45db5a2623f3ba799138cee557257
Bruno Jurkovski <bjurkovski@gmail.com>
2010-08-19 22:00:05 -0300

    just organized a few files. Working more tomorrow morning...

--- game.py
+++ game.py
@@ -1,8 +1,11 @@
 from stage import *
 
-# here goes the game logic:
-# title menu state machine, etc
-
 class Game:
 	def __init__(self):
-		pass
+		# maybe reading from a config file whats the start level...
+		# how to know the players that will be in game? a ChoosePlayer screen before the constructor?
+		self.characters = []
+		self.stage = None
+		self.room = None
+
+# to do (or not): create GameServer and GameClient classes to inherit from Game
