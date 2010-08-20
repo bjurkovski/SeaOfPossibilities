bd657621c3f276c52de4a9f729b1a8a69abf3bfa
Lucas <lfzawacki@gmail.com>
2010-08-20 11:16:30 -0300

    mouse in mapeditor

--- mapeditor.py
+++ mapeditor.py
@@ -1,29 +1,43 @@
 from pandac.PandaModules import loadPrcFileData
-loadPrcFileData("", "want-directtools #t")
-loadPrcFileData("", "want-tk #t")
+#loadPrcFileData("", "want-directtools #t")
+#loadPrcFileData("", "want-tk #t")
 
 
 from math import pi, sin, cos
 
 from direct.showbase.ShowBase import ShowBase
 from direct.task import Task
+from panda3d.core import Vec2,Vec3,Vec4
 
 from panda3d.core import CardMaker
 from direct.gui.DirectGui import *
+
+import sys
 
 class MyApp(ShowBase):
 
 	def __init__(self, w, h):
 		ShowBase.__init__(self)
 
-		#number of cells to be present in the map field
+		#Disable mouse-based camera-control
+		base.disableMouse()
 
+		#number of cells to be present in the map field
 		self.width = w
 		self.height = h
 
 		#the card generator
 		self.cardMaker = CardMaker('card')
+		self.mpos = Vec2(0,0)
 
+		self.title = OnscreenText(mayChange= True , style=1, fg=(1,1,1,1), pos=(0.5,-0.95), scale = .07)
+
+		taskMgr.add(self.mouseInput, "mouseInput")
+		taskMgr.add(self.idle,"idle")
+
+		self.accept('escape', sys.exit)
+
+	def drawBoard(self):
 		tex = loader.loadTexture('tex/tile.png')
 		for i in range(self.width):
 			for j in range(self.height):
@@ -33,19 +47,22 @@ class MyApp(ShowBase):
 				print( 1 - 2 * i * sx ,1 - 2 * j * sy ) 
 				card.setTexture(tex)
 
-
-		self.title = OnscreenText(text="MapMaker", style=1, fg=(1,1,1,1), pos=(0.5,-0.95), scale = .07)
-
-
 	def newCard( self, w, h ):
 		self.cardMaker.setFrame(w,-w,h,-h)
 		self.cardMaker.setColor(0,1,0,1)
 		return self.cardMaker.generate()
 
+	def mouseInput(self,task):
+
+		if base.mouseWatcherNode.hasMouse():
+			self.mpos = base.mouseWatcherNode.getMouse()
+
+		return Task.cont
+
 	def idle(self, task):
-				
 		#se colocar esse setPos no __init__, nao funciona...
-		self.camera.setPos(0, 0, 0)
+		#self.camera.setPos(0, 0, 0)
+		self.title.setText ("MapMaker (%.2f,%.2f)" % ( self.mpos.getX(), self.mpos.getY() ) )
 		return Task.cont
 
 
