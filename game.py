28bcb86316bf194ab24a6ac1f0be757c431fc112
Lucas <lfzawacki@gmail.com>
2010-09-01 00:18:46 -0300

    semantic keypresses

--- game.py
+++ game.py
@@ -4,15 +4,19 @@ from direct.task import Task
 from direct.actor.Actor import Actor
 from panda3d.core import Point3
 
-class Game(ShowBase):
+from input import *
+
+class Game(ShowBase,Input):
 	def __init__(self, stage, characters):
 		ShowBase.__init__(self)
+		Input.__init__(self)
+		
 		# how to know the players that will be in game? a ChoosePlayer screen before the constructor?
 		self.characters = characters
 		self.stage = stage
 		self.room = self.stage.start
 
-		self.render.attachNewNode(self.stage.maps[self.room].getNode())
+		#self.render.attachNewNode(self.stage.maps[self.room].getNode())
 		#test stuff
 		self.actor = Actor("models/panda-model", {"walk": "models/panda-walk4"})
 		self.actor.setScale(0.0005, 0.0005, 0.0005)
@@ -21,32 +25,32 @@ class Game(ShowBase):
 		self.actor.loop("walk")
 
 		#just testing movement
-		self.keys = {'w': False, 'a': False, 's': False, 'd': False}
-		self.accept('w', self.setKey, ['w', True])
-		self.accept('w-up', self.setKey, ['w', False])
-		self.accept('a', self.setKey, ['a', True])
-		self.accept('a-up', self.setKey, ['a', False])
-		self.accept('s', self.setKey, ['s', True])
-		self.accept('s-up', self.setKey, ['s', False])
-		self.accept('d', self.setKey, ['d', True])
-		self.accept('d-up', self.setKey, ['d', False])
+		#self.keys = {'w': False, 'a': False, 's': False, 'd': False}
+		keymap = { 'up' : 'w', 'down' : 's', 'left' : 'a', 'right' : 'd'}
+		self.inputMap(keymap)
+		self.bindKeys()
+		
+#		self.accept('w', self.setKey, ['w', True])
+#		self.accept('w-up', self.setKey, ['w', False])
+#		self.accept('a', self.setKey, ['a', True])
+#		self.accept('a-up', self.setKey, ['a', False])
+#		self.accept('s', self.setKey, ['s', True])
+#		self.accept('s-up', self.setKey, ['s', False])
+#		self.accept('d', self.setKey, ['d', True])
+#		self.accept('d-up', self.setKey, ['d', False])
 
 		taskMgr.add(self.idle, "Idle")
 		taskMgr.add(self.move, "Move")
 
-	#movement test:
-	def setKey(self, key, value):
-		self.keys[key] = value
-
 	def move(self, param):
 		disp = Point3(0, 0, 0)
-		if self.keys['w']:
+		if self.action['up']:
 			disp += Point3(0, 0, 0.05)
-		if self.keys['a']:
+		if self.action['left']:
 			disp += Point3(-0.05, 0, 0)
-		if self.keys['s']:
+		if self.action['down']:
 			disp += Point3(0, 0, -0.05)
-		if self.keys['d']:
+		if self.action['right']:
 			disp += Point3(0.05, 0, 0)
 
 		self.actor.setPos(self.actor.getPos() + disp)
@@ -59,3 +63,4 @@ class Game(ShowBase):
 		return Task.cont
 
 # to do (or not): create GameServer and GameClient classes to inherit from Game
+
---
+++ input.py
@@ -0,0 +1,50 @@
+"""
+	class Input
+	receives keyboard input and returns abstract action strings
+	denoting what this input means
+	
+	Just inherit from this guy and feel the delights of semantic keypresses
+""" 
+class Input():
+
+	def __init__(self):
+		#a mapping of key -> action that denotes which key is binded to which action
+		self.keymap = {}
+		
+		#a mapping of action -> bool, that represents if an action has been requested or not
+		self.action = {}
+		pass
+		
+	def inputMap(self,adict):
+		#maybe clear old keymap
+		for action,key in adict.iteritems():
+			self.mapKey(action,key)
+			self.action[self.keymap[key]] = False
+	
+	def mapKey(self, action, key):
+		"""
+			Maps a semantic string denoting an action to
+			the raw string denoting the key that is pressed for
+			this action, an example would be mapKey("jump","space")
+		"""
+		self.keymap[key] = action
+		
+	def setKey(self, key, pressed):
+		self.action[self.keymap[key]] = pressed
+		
+	def bindKeys(self):
+		"""
+			Binds the current key configuration for one object
+		"""
+		for k in self.keymap.keys():
+			self.accept(k, self.setKey, [k, True])
+			self.accept(k + '-up', self.setKey, [k, False])
+
+	def unbindKeys(self):
+		"""
+			Unbinds the current key configuration
+		"""
+		#stop receiveing events from these keys
+		pass
+
+
