cfdeff5fe20e35c24e82955a556a9306c627d471
Lucas <lfzawacki@gmail.com>
2010-10-15 02:58:08 -0200

    new render.pos in json

--- character.py
+++ character.py
@@ -18,6 +18,7 @@ class Character:
 		self.actor = Actor(self.data["render"]["model"], self.data["render"]["animation"])
 		self.actor.setScale(self.data["render"]["scale"][0], self.data["render"]["scale"][1], self.data["render"]["scale"][2])
 		self.actor.setHpr(self.data["render"]["hpr"][0], self.data["render"]["hpr"][1], self.data["render"]["hpr"][2])
+		self.actor.setPos(self.data["render"]["pos"][0], self.data["render"]["pos"][1], self.data["render"]["pos"][2])
 		self.isMoving = False
 		
 	def doAction(self, pressedKeys):
