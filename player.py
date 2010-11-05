da77ac4ca40bcd9dab0dc6fee64519a5996d612e
Apus <usuario@.(none)>
2010-11-05 16:14:54 -0200

    just adding player.py

---
+++ player.py
@@ -0,0 +1,57 @@
+from character import *
+
+class Player:
+	id = 0
+	def __init__ (self, character, id = -1) :
+		if id == -1:
+			self.id = Player.id
+			Player.id += 1
+		else :
+			self.id = id
+		
+		self.character = character
+	
+	def __eq__ (self, other) :
+		return self.id == other.id
+	
+	def sendCommand(self) :
+		#self.character.tryToMove = {}
+		self.character.tryToDo = {}
+
+
+class HumanPlayer(Player):
+	def __init__ (self, character, keys, id = -1) :
+		Player.__init__(self, character, id)
+		self.keys = keys
+	
+	def sendCommand(self) : 
+		Player.sendCommand(self)
+		
+		directions = [key for key in ["up%d"%(self.id),"down%d"%(self.id),"left%d"%(self.id),"right%d"%(self.id)] if self.keys[key]]
+		self.character.tryToMove = directions
+		
+		actions = [key for key in ["action%d"%(self.id),"attack%d"%(self.id),"cancel%d"%(self.id)] if self.keys[key]]
+		if len(actions) != 0 :
+			self.character.tryToDo = actions[0] #Soh deve executar uma action!
+		
+		
+
+		
+class ComputerPlayer(Player):
+	def __init__ (self, character, id = -1) :
+		Player.__init__(self, character, id)
+	
+	def sendCommand(self) :
+		Player.sendCommand(self)
+		directions = ["up%d"%(self.id),"right%d"%(self.id)]
+		self.character.tryToMove = directions
+		#ok, isso vai mudar!
+
+
+class ClientPlayer(Player): #comofas com os sockets?
+	def __init__ (self, character, id = -1) :
+		Player.__init__(self, character, id)
+	
+	def sendCommand(self) :
+		Player.sendCommand(self)
+		#ok, isso vai mudar!
