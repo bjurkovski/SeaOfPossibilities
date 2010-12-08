ae9841d8b413e48708a1e816a6d63542bb4c00a3
Lucas <lfzawacki@gmail.com>
2010-12-08 18:09:34 -0200

    started loader again

---
+++ gameLoader.py
@@ -0,0 +1,25 @@
+from direct.showbase.Loader import Loader
+from direct.task import Task
+
+class GameLoader():
+
+    self.textures = {}
+    self.models = {}
+    self.tracks = {}
+
+    def loadModel(filename):
+
+        try:
+
+        except:
+
+        model = Loader.loadModel(filename)
+
+        pass
+
+    def loadTexture():
+        pass
+
+    def loadMusic():
+        pass
+
